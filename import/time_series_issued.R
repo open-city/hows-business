9cc9682ef980fd9cc8e38c0a9e76e877f1de428e
Derek Eder <derek.eder@gmail.com>
2012-07-24 20:21:08 -0500

    added import scripts

---
+++ import/time_series_issued.R
@@ -0,0 +1,90 @@
+setwd('~/public/hows-business/import/')
+
+library(zoo)
+library(xts)
+print('loading issued business licenses ...')
+
+#import csv
+raw_licenses <- read.csv('../data/BusinessLicenseIssued.csv')
+
+derived_business_licenses <- c("Auto Gas Pump Certification",
+                               "Special Event Food",
+                               "Home Occupation",
+                               "Tobacco Retail Over Counter",
+                               "Consumption on Premises - Incidental Activity",
+                               "Hazardous Materials",
+                               "Package Goods",
+                               "Raffles",
+                               "Outdoor Patio",
+                               "Repossessor Class B Employee",
+                               "Explosives, Certificate of Fitness",
+                               "Caterer's Liquor License",
+                               "Expediter - Class B Employee",
+                               "Accessory Garage",
+                               "Expediter - Class B",
+                               "Retail Food Est.-Supplemental License for Dog-Friendly Areas",
+                               "Food - Shared Kitchen Long-Term User",
+                               "Class B - Indoor Special Event",
+                               "Food - Shared Kitchen Short-Term User",
+                               "Repossessor Class B",
+                               "(Other)",
+                               "Caterer's Registration (Liquor)",
+                               "Food - Shared Kitchen - Supplemental")
+
+#filter out licenses that alway require a business license
+filtered_licenses <- raw_licenses[!raw_licenses$LICENSE.DESCRIPTION %in% derived_business_licenses, c("LICENSE.DESCRIPTION", "PAYMENT.DATE")]
+
+
+#filtered_licenses <- raw_licenses[,c("LICENSE.DESCRIPTION", "PAYMENT.DATE")]
+
+names(filtered_licenses) <- c("LICENSE.DESCRIPTION", 
+                              "date")
+
+filtered_licenses$date <- factor(filtered_licenses$date)
+
+
+date_count <- table(filtered_licenses$date)
+date_count <- as.data.frame(date_count)
+names(date_count) <- c("date", "count")
+date_count <- date_count[date_count$date != "",]
+
+
+date_count$date <- as.Date(date_count$date, 
+          	                        "%m/%d/%Y")
+
+date_count <- date_count[date_count$date >= "2005-01-01",]
+date_count <- date_count[date_count$date < "2012-07-01",]
+
+date_count <- xts(date_count$count, date_count$date)
+
+month_count <- apply.monthly(date_count, sum)
+week_count <- apply.weekly(date_count, sum)
+
+month_count_ts <- ts(as.numeric(month_count),
+                     start(month_count),
+                     frequency = 12)
+
+week_count_ts <- ts(as.numeric(week_count),
+                     start(week_count),
+                     frequency = 52)
+
+decomposed_month <- stl(log(month_count_ts),
+                         "per",
+                         s.degree=1,
+                         t.window=7,
+                         robust=TRUE)
+
+plot(decomposed_month)
+
+
+decomposed_week <- stl(log(week_count_ts),
+                       s.window=5,
+                       s.degree=0,
+                       t.window=19,
+                       robust=TRUE)
+
+plot(decomposed_week)
+
+#output raw and trend data to csv
+write.csv(round(decomposed_month$time.series[,2]), "../data/trend_data_issued.csv")
+write.csv(month_count, "../data/raw_data_issued.csv")
---
+++ import/update_licences_issued.sh
@@ -0,0 +1,1 @@
+wget --output-document ../data/BusinessLicenseIssued.csv https://data.cityofchicago.org/api/views/um9x-jez2/rows.csv?accessType=DOWNLOAD
