e9e8a3665ef23d4319e41a3c4b64ba0c72a62db7
Forest Gregg <fgregg@uchicago.edu>
2012-08-23 17:58:36 -0500

    remove unused analysis files

--- import/census_api_test.py
+++
@@ -1,9 +0,0 @@
-#using the Sunlight Labs Census API wrapper: https://github.com/sunlightlabs/census
-
-from census import Census
-from us import states
-
-c = Census("395ecd145938bf526faf5070f0d3f717febbc467")
-homes1939 = c.acs.get(('NAME', 'B25034_010E'), {'for': 'state:%s' % states.MD.fips})
-
-print homes1939
--- import/update_licences_issued.sh
+++
@@ -1,1 +0,0 @@
-wget --output-document data/BusinessLicenseIssued.csv https://data.cityofchicago.org/api/views/um9x-jez2/rows.csv?accessType=DOWNLOAD
