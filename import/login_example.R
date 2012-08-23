d330fb08cfaf2018eae54bea43866282b58dfc6f
Forest Gregg <fgregg@uchicago.edu>
2012-08-23 08:32:59 -0500

    login example

--- import/login_example.R
+++ import/login_example.R
@@ -1,4 +1,7 @@
 # Google Account Info for Fusion Table Access
+#
+# Replace with your account information and save file as login.R
+
 login.username = 'asdasdf'
 login.password = 'fadsfas'
 login.table_id = '1_RnACjjyI4qmAaOQyRFB2W5iItGnvbqNaymnEW4'
