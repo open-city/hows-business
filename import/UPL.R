7a2b6456b3b870fb7b6528e59454096429442e63
Forest Gregg <fgregg@uchicago.edu>
2012-08-23 11:45:42 -0500

    refactored unemployment

--- import/UPL.R
+++ import/UPL.R
@@ -1,38 +1,26 @@
-upl <- read.csv("cook_county_upl.csv")
-upl_ts <-ts(upl[,2],start=1990,frequency=12)
+library(tframe)
 
-Months <- c(rep(c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep',"Oct",'Nov','Dec'),22),c('Jan','Feb','Mar','Apr','May','Jun'))
-upl.time <- 1:length(upl_ts)
-upl.df.month <- data.frame(time,Months)
-upl.df.month.2 <- data.frame(time,time^2,Months)
-upl.df.month.3 <- data.frame(time,time^2,time^3,Months)
-upl.df.month.4 <- data.frame(time,time^2,time^3,time^4,Months)
+unemployment.df <- read.table('http://research.stlouisfed.org/fred2/data/ILCOOK1URN.txt',
+                                   skip=11,
+                                   header = TRUE)
 
-#Box-Cox Test tells us to take inverse square root of the response.
-boxcox(lm(upl_ts~.,data=upl.df.month))
-#But log when time^2 and time ^3 are added.
+names(unemployment.df) <- c("date", "value")
+unemployment.df$date <- as.Date(unemployment.df$date, "%Y-%m-%d")
+unemployment.df$month <- months(unemployment.df$date)
 
-#Full Linear Model
-upl.month <- lm(1/sqrt(upl_ts)~.,data=upl.df.month)
-summary(upl.month) #R^2 is pathetic.
-upl.month.2 <- lm(log(upl_ts)~.,data=upl.df.month.2)
-summary(upl.month.2)
-upl.month.3 <- lm(log(upl_ts)~.,data=upl.df.month.3)
-summary(upl.month.3)
-upl.month.4 <- lm(log(upl_ts)~.,data=upl.df.month.4)
-summary(upl.month.4)
+unemployment.ts <-ts(unemployment$value,
+                     start=c(1990, 1),
+                     frequency=12)
+
 
 #Take the stl decomposition and then linear and cubic models
 upl_stl <- stl(log(upl_ts),s.window=7,s.degree=1,t.window=9,robust=TRUE)
+
 upl_trend <- upl_stl$time.series[,'trend']
 upl_truncate <- tfwindow(upl_trend,start=c(2005,1))
-upl.lm <- lm(upl_truncate~time)
-upl.lm.3 <- lm(upl_truncate~time+I(time^2)+I(time^3))
-#Cubic and linear models work best
 
+plot(upl_truncate)
 
-#####################################################################
-upl_decomp <- decompose(upl_ts,type=c('additive'))
 
 
 plot(upl_trend)
