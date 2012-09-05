d35c499bcbb9e63d2c2388b7ab67cf8d7772ccaa
Forest Gregg <fgregg@uchicago.edu>
2012-09-05 11:52:29 -0500

    added another year to foreclosure data

--- import/foreclosure.R
+++ import/foreclosure.R
@@ -4,12 +4,12 @@ source('login.R')
 foreclosures <- read.csv("data/chicago_foreclosures.csv")
 
 foreclosure.ts <- ts(colSums(foreclosures[,3:dim(foreclosures)[2]]),
-                     start = c(2007, 1),
+                     start = c(2006, 1),
                      frequency = 4)
 
 auth = ft.connect(login.username, login.password)
 quarterly_data = paste(foreclosure.ts, collapse=",")
-quarterly_data = paste(paste(rep(',', 8), collapse=""), quarterly_data, sep='')
+quarterly_data = paste(paste(rep(',', 4), collapse=""), quarterly_data, sep='')
 quarterly_data = paste(quarterly_data, "," sep='')
 updateFT(auth, login.table_id, 'Foreclosure Quarterly', quarterly_data)
 
