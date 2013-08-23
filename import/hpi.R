3da3266765354c4ef8d266354a7aad02ff9976a7
Forest Gregg <fgregg@uchicago.edu>
2013-08-23 14:09:09 -0500

    Housing Price Index Script

---
+++ import/hpi.R
@@ -0,0 +1,27 @@
+library(tframe)
+
+hpi <- read.csv("data/q1_2013_ihs_house_price_index_tabular_data.csv")
+
+hpi.ts <- ts(hpi$Single.Family,
+             start = c(1997, 1),
+             frequency = 4)
+hpi.ts <- tframe::tfwindow(hpi.ts, start=c(2005,1))
+
+quarterly_data = paste(hpi.ts, collapse=",")
+
+hpi <- paste(
+ 'var hpi = {"grouping" : "HPI",
+ "Title" : "Cook County Housing Price Index",
+ "Source" : "Depaul Institute of Housing Studies",
+ "Label" : "HPI",
+ "Start Year" : 2005,
+ "Data Type" : "count",
+ "Point Interval" : "quarter",
+ "Data Raw" : [], 
+ "Data Trend" : [',
+  quarterly_data,
+  ']}')
+
+write(hpi, "hpi.js")
+
+
